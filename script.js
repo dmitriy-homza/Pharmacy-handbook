2d67b5c54b1473f39fc31bc7a586787a5dcd86c7
dmitriy-homza <54332269+dmitriy-homza@users.noreply.github.com>
2020-01-30 15:27:42 +0300

    js?

--- script.js
+++ script.js
@@ -223,7 +223,7 @@ function openTabs(el) {
    btnTarget.classList.add("active");
 }
 
-//Таблица
+//Таблица анимация
 // '.tbl-content' consumed little space for vertical scrollbar, scrollbar width depend on browser/os/platfrom. Here calculate the scollbar width .
 $(window).on("load resize ", function() {
   var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
